ae469eaa53a97cc5d94418c87fff4c3e6789fde9
MD MOYNUL ISLAM <99502593+MOYNUL75@users.noreply.github.com>
2026-01-31 12:43:40 +0600

    Add C++ basics programs

--- Basics/basics_13_for_loop.cpp
+++ Basics/basics_13_for_loop.cpp
@@ -36,6 +36,62 @@ int main(){
     }
     cout << "Sum of numbers from 1 to 100 is: " << sum << endl;
 
+    // Multiplication Table of 7 using For Loop
+    int number = 7;
+    cout << "Multiplication Table of " << number << " is:" << endl;
+    for(int i = 1; i <= 10; i++){
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
 
+    // Nested For Loop Example 01
+    cout << "Nested For Loop Example 01" << endl;
+    for(int i = 1; i <= 3; i++){
+        for(int j = 1; j <= 2; j++){
+            cout << "i: " << i << ", j: " << j << endl;
+        }
+    }
+
+    // Nested For Loop Example 02
+    cout << "Nested For Loop Example 02" << endl;
+    for(int i = 1; i <= 4; i++){
+        for(int j = 1; j <= 3; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // Nested For Loop Example 03 - Print a 5x5 Grid of Numbers
+    cout << "Nested For Loop Example 03" << endl;
+    cout << "5x5 Grid of Numbers:" << endl;
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= 5; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+
+    // Nested For Loop Example 04
+    cout << "Nested For Loop Example 04" << endl;
+    for (int i = 1; i <= 3; i++){
+        for(int j = 1; j <=3; j++){
+            cout<<i * j<<" ";
+        }
+        cout<<endl;
+    }
+
+    // The foreach Loop (Range-based for loop)
+    /* Syntax:
+         for (type variableName : arrayName) {
+         // code block to be executed
+        }
+    */
+    cout << "Range-based For Loop Example 01" << endl;
+    int numarray[] = {10, 20, 30, 40, 50};
+    for(int num : numarray){
+        cout <<" Number: " << numarray << endl;
+    }
+
+    
     return 0;
 }
